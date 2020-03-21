8a05d4b9b14d59f75e1ba38f6d8f30cdd4d072f2
Hendrik Sebastian Voß <hendrik@familievoss.org>
2020-03-21 19:19:32 +0100

    Css New Question

---
+++ components/shared/Card.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {StyleSheet, View} from "react-native";
+import { black } from "./StylesAndColors";
+
+export default function Card({children, style = {}}) {
+  return <View style={[styles.card, style]}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "space-around",
+    alignSelf: 'stretch',
+    marginHorizontal: 20,
+    alignContent: "center",
+    padding: 10,
+    borderColor: '#ddd',
+    shadowColor: black,
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 5,
+    elevation: 2,
+  }
+});
