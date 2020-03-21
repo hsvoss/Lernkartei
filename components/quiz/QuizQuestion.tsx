f7bb302023ea6a58d73359212ebb5d98a2fd4569
Hendrik Sebastian Voß <hendrik@familievoss.org>
2020-03-21 22:05:44 +0100

    Css QuizQuestionView

--- components/quiz/QuizQuestion.tsx
+++ components/quiz/QuizQuestion.tsx
@@ -1,28 +1,32 @@
 import React from "react";
-import {KeyboardAvoidingView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import {
-  blue,
-  centerWhite,
-  white,
-  black,
-  gray, green, red
-} from "../shared/StylesAndColors";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {black, blue, centerWhite, green, red} from "../shared/StylesAndColors";
 import TextButton from "../shared/TextButton";
 import Card from "../shared/Card";
+import CardFlip from 'react-native-card-flip';
 
 export function QuizQuestion() {
   return (
     <View style={centerWhite.container}>
-      <Card>
-        <Text style={styles.header}>Does React Native work with Android?</Text>
-      </Card>
-      <TouchableOpacity onPress={() => {}}>
+      <CardFlip style={styles.cardContainer} ref={card => (this.card = card)}>
+        <Card style={styles.card}>
+          <Text style={styles.header}>Does React Native work with Android? Does React Native work with Android?</Text>
+        </Card>
+        <Card style={styles.card}>
+          <Text style={styles.header}>Yes!</Text>
+        </Card>
+      </CardFlip>
+      <TouchableOpacity onPress={() => {
+        this.card.flip()
+      }}>
         <Text style={styles.reset}>Show Answer</Text>
       </TouchableOpacity>
-      <TextButton onPress={() => {}} buttonStyle="primary" style={{backgroundColor: green}}>
+      <TextButton onPress={() => {
+      }} buttonStyle="primary" style={{backgroundColor: green}}>
         Correct
       </TextButton>
-      <TextButton onPress={() => {}} buttonStyle="primary" style={{backgroundColor: red}}>
+      <TextButton onPress={() => {
+      }} buttonStyle="primary" style={{backgroundColor: red}}>
         Wrong
       </TextButton>
     </View>
@@ -30,17 +34,22 @@ export function QuizQuestion() {
 }
 
 const styles = StyleSheet.create({
+
+  cardContainer: {
+    // flex:1,
+    width: 350,
+    height: 300,
+  },
   reset: {
     color: blue
   },
+  card: {
+    flex: 1
+  }
+  ,
   header: {
     fontSize: 40,
     color: black,
     textAlign: "center"
   },
-  cardNr: {
-    fontSize: 30,
-    color: gray,
-    textAlign: "center"
-  }
 });
