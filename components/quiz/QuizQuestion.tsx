4406b097f20216bc887c2345d3756ee7c448cace
Hendrik Sebastian Voß <hendrik@familievoss.org>
2020-03-22 22:38:41 +0100

    QuizView

--- components/quiz/QuizQuestion.tsx
+++ components/quiz/QuizQuestion.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import {View} from "react-native";
 import {centerWhite} from "../shared/StylesAndColors";
 import {QuizSingeView} from "./QuizSingeView";
+import {getDeck} from "../../model/LocalStore";
 
-export function QuizQuestion() {
+export function QuizQuestion({route, navigation}) {
+  const {deckTitel}: { deckTitel: string } = route.params;
+  const deck = getDeck(deckTitel);
+  let question = deck.questions[0];
+
+
+
   return (
     <View style={centerWhite.container}>
       <QuizSingeView
-        questionText={"Does React Native work with Android?"}
-        answerText={"Yes!"}
+        questionText={question.questionText}
+        answerText={question.answerText}
         onCorrect={() => console.log('Correct')}
         onWrong={() => console.log('Wrong')}
       />
